6adc962d4703e5c18d305a49fa406157cf0aed1c
nissano <nissano@wix.com>
2024-02-12 00:51:58 +0200

    2 - get

---
+++ data.js
@@ -0,0 +1,10 @@
+const adiSubscription = {
+  id: 1,
+  name: 'Ultimate Fitness Package',
+  cycle: 'Monthly',
+  customerName: 'Adi Harif 🦈',
+  type: 'Green 💚',
+  paymentToken: '123-456-789',
+};
+
+export const subscriptions = [adiSubscription, adiSubscription];
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log('hello world! :)');
+import http from 'http';
+import { subscriptions } from './data.js';
+
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.setHeader('Content-Type', 'application/json');
+
+  // Check for the route and method
+  if (req.url === '/subscription' && req.method === 'GET') {
+    res.writeHead(200); // HTTP status code 200: OK
+    res.end(JSON.stringify(subscriptions[0]));
+  } else {
+    // Handle not found
+    res.writeHead(404); // HTTP status code 404: Not Found
+    res.end(JSON.stringify({ message: 'Resource not found' }));
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
