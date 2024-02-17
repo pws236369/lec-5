7d43961aa1e4bf7f8b62bb7e2f02420c6be0cadc
nissano <nissano@wix.com>
2024-02-18 00:27:35 +0200

    simple getCookie

--- script.js
+++ script.js
@@ -31,14 +31,9 @@ function setCookie(name, value, days) {
 }
 
 function getCookie(name) {
-  let nameEQ = name + '=';
-  let ca = document.cookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
 function setTitleColorFromCookie() {
